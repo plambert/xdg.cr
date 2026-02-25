fa13b3788ec417c26f52ad3d05317460680d8adf
Paul M. Lambert <plambert@plambert.net>
2026-02-24 16:52:34 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# freedesktop
+
+Simple implementation of the [freedesktop.org](https://freedesktop.org) [XDG Base Directory Specification](https://specifications.freedesktop.org/basedir/latest/)
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     freedesktop:
+       github: plambert/freedesktop.cr
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "freedesktop"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/plambert/freedesktop.cr/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Paul M. Lambert](https://github.com/plambert) - creator and maintainer
---
+++ src/freedesktop.cr
@@ -0,0 +1,68 @@
+# Provide XDG base directories on a POSIX or POSIX-like system
+module Freedesktop
+  VERSION     = {{ `shards version {{__DIR__}}`.chomp.stringify }}
+  VALUE_CACHE = {} of String => Path
+  LIST_CACHE  = {} of String => Array(Path)
+
+  extend self
+
+  private def cached_value(env_var, default)
+    if cached_value = VALUE_CACHE[env_var]?
+      return cached_value
+    end
+
+    if env_value = ENV[env_var]?
+      VALUE_CACHE[env_var] = Path[env_value]
+      return VALUE_CACHE[env_var]
+    end
+
+    path = Path[default].expand(home: true)
+    Dir.mkdir_p path, mode: 0o700 unless File.directory? path
+    VALUE_CACHE[env_var] = path
+    path
+  end
+
+  private def cached_list(env_var, default)
+    if cached_value = LIST_CACHE[env_var]?
+      return cached_value
+    end
+
+    if env_value = ENV[env_var]?
+      LIST_CACHE[env_var] = env_value.split(':').map { |path| Path[path].expand }
+      return LIST_CACHE[env_var]
+    end
+
+    paths = default.split(':').map { |path| Path[path].expand }
+    LIST_CACHE[env_var] = paths
+    paths
+  end
+
+  def xdg_data_home
+    cached_value "XDG_DATA_HOME", "~/.local/share"
+  end
+
+  def xdg_config_home
+    cached_value "XDG_CONFIG_HOME", "~/.cache"
+  end
+
+  def xdg_state_home
+    cached_value "XDG_STATE_HOME", "~/.local/state"
+  end
+
+  def xdg_data_dirs
+    cached_list "XDG_DATA_DIRS", "/usr/local/share/:/usr/share/"
+  end
+
+  def xdg_config_dirs
+    cached_list "XDG_CONFIG_DIRS", "/etc/xdg"
+  end
+
+  def xdg_runtime_dir
+    dir = cached_value "XDG_RUNTIME_DIR", "#{Dir.tempdir}/.xdg_runtime_dir"
+    unless File.directory? dir
+      Dir.mkdir_p dir, mode: 0o700
+      File.chmod(dir, 0o700)
+    end
+    dir
+  end
+end
